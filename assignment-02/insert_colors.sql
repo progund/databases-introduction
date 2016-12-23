cacb180fe0d5e415bd3acd649c4616ef179c49b8
Rikard Fröberg <rikard@morus.se>
2016-12-23 19:13:24 +0100

    Files for the assignment 2

---
+++ assignment-02/insert_colors.sql
@@ -0,0 +1,8 @@
+INSERT INTO colors (color) VALUES('Blue');
+INSERT INTO colors (color) VALUES('Red');
+INSERT INTO colors (color) VALUES('Green');
+INSERT INTO colors (color) VALUES('Yellow');
+INSERT INTO colors (color) VALUES('Black');
+INSERT INTO colors (color) VALUES('White');
+INSERT INTO colors (color) VALUES('Brown');
+INSERT INTO colors (color) VALUES('Silver');
