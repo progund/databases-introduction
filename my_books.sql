8e81391481f964d4ac58fd0c425b2a7f93c7b09c
Rikard Fröberg <rikard@morus.se>
2018-01-19 14:01:47 +0100

    The SQL file for the live coding video

---
+++ my_books.sql
@@ -0,0 +1,9 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS books(Author text, Title text, ISBN text primary key, Publisher text);
+INSERT INTO "books" VALUES('John Smith','Life','0-0-0-0-0-1','Bonnier');
+INSERT INTO "books" VALUES('James Woody','Love','0-0-0-0-0-2','Bonnier');
+INSERT INTO "books" VALUES('Joan Carmen','Guns','0-0-0-0-0-3','Bonnier');
+INSERT INTO "books" VALUES('Johnanna Boyd','Code','0-0-0-0-0-4','Bonnier');
+INSERT INTO "books" VALUES('Eva Peron','Cars','0-0-0-0-0-5','Books R us');
+COMMIT;
