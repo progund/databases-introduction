c8a3aead0896afa7c80b4b7ff1b5039248a39c03
Rikard Fröberg <rikard@morus.se>
2018-11-15 11:25:23 +0100

    Coding style

--- SQL-JOIN/example-databases/decomposing-table/ALL_ABOUT_BOOK_VIEW.sql
+++ SQL-JOIN/example-databases/decomposing-table/ALL_ABOUT_BOOK_VIEW.sql
@@ -1,1 +1,10 @@
-CREATE VIEW ALL_ABOUT_BOOK AS SELECT title, book.isbn, a.name AS author_name, publisher.name as pub FROM book NATURAL JOIN book_author NATURAL JOIN author a NATURAL JOIN book_publisher JOIN publisher ON book_publisher.publisher_id = publisher.publisher_id;
+CREATE VIEW ALL_ABOUT_BOOK AS
+      SELECT title, book.isbn,
+             a.name AS author_name,
+             publisher.name AS pub
+        FROM book
+NATURAL JOIN book_author
+NATURAL JOIN author a
+NATURAL JOIN book_publisher
+        JOIN publisher
+          ON book_publisher.publisher_id = publisher.publisher_id;
