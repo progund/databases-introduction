9452a580e6421cc2610cc36eed74b7424d403edb
Rikard Fröberg <rikard@morus.se>
2019-02-09 04:19:24 +0100

    more readable

--- scraping-data/examples/curiculum/course_codes.sh
+++ scraping-data/examples/curiculum/course_codes.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-GET 'https://ait.gu.se/utbildning/program/systemvetenskap/om-programmet' | grep pdf|tr '"' '\n' | grep http|grep pdf|grep TIG|tr '/' '\n'|grep TIG|cut -d '.' -f1
+GET 'https://ait.gu.se/utbildning/program/systemvetenskap/om-programmet' |
+    grep pdf |
+    tr '"' '\n' |
+    grep http |
+    grep pdf |
+    grep TIG |
+    tr '/' '\n' |
+    grep TIG |
+    cut -d '.' -f1
