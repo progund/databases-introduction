91b00df978221cb0d11794882bee92ed8fce4ea3
Rikard Fröberg <rikard@morus.se>
2019-02-09 03:41:08 +0100

    Better learnign outcomes

--- scraping-data/examples/curiculum/curiculum.sh
+++ scraping-data/examples/curiculum/curiculum.sh
@@ -6,4 +6,4 @@ pdftotext "$CODE.pdf"
 echo "${CODE}"
 fmt -200 "$CODE.txt"|tr ' ' '\n'|egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}' | sed -e 's/[.,]//g'
 echo "Lärandemål"
-cat "$CODE.txt" | grep '•'
+cat "$CODE.txt" |grep .| grep -A 19 'Kunskap och'|egrep -A3 'Kunskap och|Färdigheter|Värderingsför|•'|egrep -v '^Innehåll|Kursen|^[0-9]|^Se '
