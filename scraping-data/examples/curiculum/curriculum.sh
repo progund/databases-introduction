a4c4912733c47017becaa3801bd67f9c555c5df9
Rikard Fröberg <rikard@morus.se>
2019-02-09 04:21:07 +0100

    More readable

--- scraping-data/examples/curiculum/curriculum.sh
+++ scraping-data/examples/curiculum/curriculum.sh
@@ -4,6 +4,20 @@ CODE=$1
 wget -q "http://kursplaner.gu.se/pdf/kurs/sv/$CODE" -O "$CODE.pdf"
 pdftotext "$CODE.pdf"
 echo "${CODE}"
-fmt -200 "$CODE.txt"|tr ' ' '\n'|egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}' | sed -e 's/[.,]//g'
+fmt -200 "$CODE.txt" |
+    tr ' ' '\n' |
+    egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}' |
+    sed -e 's/[.,]//g'
+YEAR=$(fmt -200 "$CODE.txt" |
+              tr ' ' '\n' |
+              egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}' |
+              sed -e 's/[.,]//g' |
+              head -1)
+echo "Age: $(units $(date +%s)sec-$(date -d "$YEAR" +%s)sec 'yr;mo;d')"
 echo "Lärandemål"
-cat "$CODE.txt" |grep .| grep -A 19 'Kunskap och'|egrep -A3 'Kunskap och|Färdigheter|Värderingsför|•'|egrep -v '^Innehåll|Kursen|^[0-9]|^Se '
+cat "$CODE.txt" |
+    grep .|
+    grep -A 19 'Kunskap och' |
+    egrep -A3 'Kunskap och|Färdigheter|Värderingsför|•' |
+    egrep -v '^Innehåll|Kursen|^[0-9]|^Se '
+
