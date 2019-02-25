aeb853d7d2480f3865a4c53d805155f0b2574385
Rikard Fröberg <rikard@morus.se>
2019-02-25 12:16:11 +0100

    validate args

--- scraping-data/examples/various-gu/get_gu_id.sh
+++ scraping-data/examples/various-gu/get_gu_id.sh
@@ -2,6 +2,13 @@
 
 name1="$1"
 name2="$2"
+
+if (( $# != 2))
+then
+    echo "Usage: ./$0 <firstname> <lastname>"
+    exit 1
+fi
+
 URL='https://ait.gu.se/om-institutionen/?selectedTab=2&itemsPerPage=-1'
 GET "$URL" |
     awk '/<tbody>/,/<\/tbody>/' |
